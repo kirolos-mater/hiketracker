99caa94008444c51232701f3d168889c7b7b2ae8
h4rk <ale.sala.97@gmail.com>
2020-10-16 15:06:39 +0200

    Preparato user service con metodi per login e registrazione

--- src/main/java/it/beije/hiketracker/repositories/UserRepository.java
+++ src/main/java/it/beije/hiketracker/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package it.beije.hiketracker.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import it.beije.hiketracker.entitybeans.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
+	Optional<User> findByEmailAndPassword(String email, String password);
+	
+	Optional<User> findByEmail(String email);
 }
